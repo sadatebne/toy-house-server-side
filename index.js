ece8be020b7025ceabb15746131fb4fe566c4283
sadatebne <sadatshafin75@gmail.com>
2023-05-20 10:50:17 +0600

    get req updatedt

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port=process.env.PORT || 3000
 
 require('dotenv').config()
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -14,7 +14,6 @@ app.get('/',(req,res)=>{
     res.send('hello world')
 })
 
-console.log(process.env.DB_USER)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsw32ip.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -31,6 +30,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("toyHouseDB");
+    const toysCollection=database.collection("allToys");
+
+    app.get('/allToys', async(req,res)=>{
+        const cursor = toysCollection.find({});
+        const result = await cursor.toArray();
+        res.send(result)
+
+    })
+
+    app.get('/allToys/:id', async(req,res)=>{
+        const id=req.params.id
+        const query ={_id: new ObjectId(id)}
+        const result = await toysCollection.findOne(query);
+        res.send(result)
+
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
